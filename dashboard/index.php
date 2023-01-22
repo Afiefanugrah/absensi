e72637f17b119de4f56dae2dd42ac12713a87fe4
M AFIEF ANUGRAH <mafiefanugarh01@gmail.com>
2023-01-22 08:26:08 +0700

    testing first

---
+++ dashboard/index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$user_id = $_SESSION['user_id'];
+$nama_lengkap = $_SESSION['nama_lengkap'];
+$role = $_SESSION['role'];
+$status = $_SESSION['status'];
+
+if ($status != "login") {
+    header("location:../index.php?message=silakan login terlebih dahulu!");
+}
+if (isset($_POST['logout'])) {
+    session_destroy();
+    header("location:../index.php?terimah kasih sudah berkunjung");
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DASBOARD</title>
+</head> 
+<body>
+    <h3>Dasboard</h3>
+    <p>
+        <?php
+        if (isset($_GET['message'])) {
+            echo $_GET['message'];
+        }
+
+        ?>
+    </p>
+    <i>Halo <?= $nama_lengkap ?></i>
+    <p>Status kepergawaian : <?= $role ?></p>
+    <br/>
+    <form action="" method="POST">
+        <button type="submit" name="logout">Logout</button>
+    </form>
+</body>
+</html>
